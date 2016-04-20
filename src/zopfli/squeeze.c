efae472220e04c74c3ef45e8b3015e17fbba608d
Lode Vandevenne <lvandeve@users.noreply.github.com>
2016-04-20 17:16:24 +0200

    Update squeeze.c

--- src/zopfli/squeeze.c
+++ src/zopfli/squeeze.c
@@ -196,8 +196,7 @@ static double GetCostModelMinCost(CostModelFun* costmodel, void* costcontext) {
   return costmodel(bestlength, bestdist, costcontext);
 }
 
-static size_t min(size_t a, size_t b)
-{
+static size_t min(size_t a, size_t b) {
   return a < b ? a : b;
 }
 
